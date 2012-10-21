e048a4f7e973057b7952865070173aa9165f0ff4
Alex Lambert <alex@bifflabs.com>
2012-10-21 10:46:48 -0400

    cleanup

--- examples/helloworld/src/main/java/com/spindle/otto/helloworld/Main.java
+++ examples/helloworld/src/main/java/com/spindle/otto/helloworld/Main.java
@@ -11,18 +11,21 @@ public final class Main {
 
     private final static Logger logger = LoggerFactory.getLogger(Main.class);
 
-    public static void main(String[] args) throws InterruptedException {
+    public static void main(String[] args) {
         logger.trace("Logging at trace level; edit logback.xml to change the log level");
         logger.debug("Logging at debug level; edit logback.xml to change the log level");
         logger.info("Logging at info level; edit logback.xml to change the log level");
 
         Config config = ConfigFactory.load();
-        logger.info("Configuration value1: " + config.getString("helloworld.value1"));
-        logger.info("Configuration value2: " + config.getString("helloworld.value2"));
-        logger.info("Configuration value3: " + config.getString("helloworld.value3"));
+        logger.info("Configuration value1: {}", config.getString("helloworld.value1"));
+        logger.info("Configuration value2: {}", config.getString("helloworld.value2"));
+        logger.info("Configuration value3: {}", config.getString("helloworld.value3"));
 
         logger.info("Sleeping for 30 seconds and then exiting to simulate an application crash");
-        TimeUnit.SECONDS.sleep(30);
+        try {
+            TimeUnit.SECONDS.sleep(30);
+        } catch (InterruptedException e) {
+        }
         logger.info("Exiting to simulate an application crash");
     }
 }
