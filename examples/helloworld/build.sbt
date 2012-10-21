17c8f2e93568e0255c4dd0b7d1e6df3920006692
Alex Lambert <alex@bifflabs.com>
2012-10-21 01:36:00 -0400

    configuration

--- examples/helloworld/build.sbt
+++ examples/helloworld/build.sbt
@@ -10,4 +10,6 @@ libraryDependencies += "org.slf4j" % "slf4j-api" % "1.7.2"
 
 libraryDependencies += "ch.qos.logback" % "logback-classic" % "1.0.7"
 
+libraryDependencies += "com.typesafe" % "config" % "1.0.0"
+
 assemblySettings
--- examples/helloworld/src/main/scala/com/spindle/otto/helloworld/Main.scala
+++ examples/helloworld/src/main/scala/com/spindle/otto/helloworld/Main.scala
@@ -2,14 +2,18 @@ package com.spindle.otto.helloworld
 
 import org.slf4j.LoggerFactory
 import java.util.concurrent.TimeUnit
+import com.typesafe.config._
 
 object Main {
   private val logger = LoggerFactory.getLogger(getClass)
 
   def main(args: Array[String]) {
-    logger.trace("Logging at trace level")
-    logger.debug("Logging at debug level")
-    logger.info("Logging at info level")
+    logger.trace("Logging at trace level; edit logback.xml to change the log level")
+    logger.debug("Logging at debug level; edit logback.xml to change the log level")
+    logger.info("Logging at info level; edit logback.xml to change the log level")
+
+    val config = ConfigFactory.load()
+    logger.info("From configuration: " + config.getString("helloworld.text"))
 
     logger.info("Sleeping for 30 seconds and then exiting to simulate an application crash")
     TimeUnit.SECONDS.sleep(30)
