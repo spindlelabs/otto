cb8e32ba5717cc22e4769c0a9f6e77acf53dcb3f
Alex Lambert <alex@spindle.com>
2012-10-22 12:51:01 -0400

    Fix name

--- examples/helloworld/modules/helloworld/manifests/init.pp
+++ examples/helloworld/modules/helloworld/manifests/init.pp
@@ -1,5 +1,5 @@
 class helloworld($jenkinsProjectName, $jenkinsBuildID, $value2) {
-  ottoexample1::otto_java_app_from_jenkins { "helloworld":
+  otto_java_app_from_jenkins { "helloworld":
     jenkinsProjectName => $jenkinsProjectName,
     jenkinsBuildID => $jenkinsBuildID,
     appRunContent => template("helloworld/run.erb"),
