dd1911eb95eb253421de2627ba151058722b59f1
Alex Lambert <alex@spindle.com>
2012-10-24 18:27:15 -0400

    move documentation

--- examples/helloworld/manifests/site.pp
+++ examples/helloworld/manifests/site.pp
@@ -4,11 +4,7 @@
 node default {
   class { "helloworld":
     jenkinsProjectName => "helloworld-master-checkin",
-    # You could specify the build in the node manifest:
     jenkinsBuildID => "1",
-    # Or, you could could use generate(), extlookup(), or an external node
-    # classifier to determine the build to run. You can use this to
-    # implement continuous deployment or automatic rollback.
     value2 => "from machine manifest"
   }
 }
